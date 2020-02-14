0e24a9d587af91f550cbf9ce360f385ed713f7ac
Dalwyx <dalwyx@gmail.com>
2020-02-14 21:15:28 +0300

    GameOver activator added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,24 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 2f;
     [SerializeField] private float _walkingAreaRadius = 4f;
+    public event Action<Enemy> EnemyDied;
     private Vector3 _currentTarget;
 
     private void Start()
     {
         SetNewDestination();
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        var player = collision.GetComponent<Player>();
-        if (player != null)
-        {
-            player.ActivateBonus();
-            Destroy(gameObject);
-        }
     }
 
     private void Update()
@@ -30,8 +22,19 @@ public class Enemy : MonoBehaviour
             SetNewDestination();
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player != null)
+        {
+            player.ActivateBonus();
+            EnemyDied?.Invoke(this);
+            Destroy(gameObject);
+        }
+    }
+
     public void SetNewDestination()
     {
-        _currentTarget = Random.insideUnitCircle * _walkingAreaRadius;
+        _currentTarget = UnityEngine.Random.insideUnitCircle * _walkingAreaRadius;
     }
 }
---
+++ Assets/Scripts/GameOverActivator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class GameOverActivator : MonoBehaviour
+{
+    private SpriteRenderer _sr;
+    private List<Enemy> _enemies;
+
+    private void Awake()
+    {
+        _sr = GetComponent<SpriteRenderer>();
+        _sr.enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        _enemies = new List<Enemy>(FindObjectsOfType<Enemy>());
+        foreach (var enemy in _enemies)
+        {
+            enemy.EnemyDied += CheckGameOverCondition;
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var enemy in _enemies)
+        {
+            enemy.EnemyDied -= CheckGameOverCondition;
+        }
+    }
+
+    public void CheckGameOverCondition(Enemy diedEnemy)
+    {
+        _enemies.Remove(diedEnemy);
+        if (_enemies.Count == 0)
+        {
+            ActivateGameOverScreen();
+        }
+    }
+
+    private void ActivateGameOverScreen()
+    {
+        _sr.enabled = true;
+    }
+}
