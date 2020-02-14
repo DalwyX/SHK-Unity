586671eca1dd41334b736b22a2867abd990c4565
Dalwyx <dalwyx@gmail.com>
2020-02-14 20:10:39 +0300

    Enemy class refactored

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 2f;
+    [SerializeField] private float _walkingAreaRadius = 4f;
+    private Vector3 _currentTarget;
+
+    private void Start()
+    {
+        SetNewDestination();
+    }
+
+    private void Update()
+    {
+        var maxDistanceDelta = _moveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, maxDistanceDelta);
+
+        if (transform.position == _currentTarget)
+            SetNewDestination();
+    }
+
+    public void SetNewDestination()
+    {
+        _currentTarget = Random.insideUnitCircle * _walkingAreaRadius;
+    }
+}
